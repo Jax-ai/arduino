08dfd862e7fb9ca733baac3fa0bd86843263c777
Jax-ai <jacques@tesseract-cloud.co.za>
2022-07-06 22:33:53 +0200

    login process

--- startup/startup.ino
+++ startup/startup.ino
@@ -17,12 +17,43 @@ void loop() {
   if(pushed == false) {
 
     //Wait 1 minute for computer to boot up
-    delay(10000);
-    Keyboard.press('n');
-    delay(100);
-    Keyboard.releaseAll();
-
+    delay(35000);
+     Keyboard.press(KEY_RETURN);
+    loginToComputer();
     pushed = true;
   }
 
+}
+
+
+void loginToComputer() {
+
+    //Press enter
+    Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+    
+    
+    //Enter Password
+    Keyboard.press('1');
+    delay(100);
+    Keyboard.releaseAll();
+    
+    Keyboard.press('2');
+    delay(100);
+    Keyboard.releaseAll();
+    
+    Keyboard.press('3');
+    delay(100);
+    Keyboard.releaseAll();
+    
+    Keyboard.press('4');
+    delay(100);
+    Keyboard.releaseAll();
+    
+    //Submit Password
+    
+     Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
 }

aca694d2d9273c09ef294d989b8cbd3177afbda7
Jax-ai <jacques@tesseract-cloud.co.za>
2022-07-06 22:03:49 +0200

    added the start script

---
+++ leo/startup.ino
@@ -0,0 +1,26 @@
+
+#include <Keyboard.h>
+
+bool pushed = false;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Keyboard.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(pushed == false) {
+    delay(1000);
+    Keyboard.press('n');
+    delay(100);
+    Keyboard.releaseAll();
+
+    pushed = true;
+  }
+
+}

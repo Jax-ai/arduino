f3cc053e0600f0599e3a36f139507a011d42f0cc
Jax-ai <jacques@tesseract-cloud.co.za>
2022-07-07 09:38:16 +0200

    fixes

--- startup/startup.ino
+++ startup/startup.ino
@@ -19,7 +19,7 @@ void loop() {
   if(pushed == false) {
 
     //Wait 1 minute for computer to boot up
-    delay(35000);
+    //delay(35000);
 
     //Login     
     loginToComputer();
@@ -28,7 +28,7 @@ void loop() {
     openMSI();
 
     //Open Miner
-    openMinder();
+   // openMinder();
 
 
 
@@ -78,20 +78,28 @@ void openMSI() {
   //Open directory  
   Keyboard.press(KEY_LEFT_GUI);
   delay(100);
+ Keyboard.releaseAll();
+ delay(100);
+    Keyboard.press(KEY_RETURN);
+  delay(100);
   Keyboard.press('R');
   delay(100);
   Keyboard.releaseAll();
-
+ Keyboard.press(KEY_RETURN);
+  delay(100);
   delay(100);
   Keyboard.print("C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe");
+delay(500);
+ Keyboard.press(KEY_RETURN);
+ Keyboard.releaseAll();
+ delay(500);
+ Keyboard.press(KEY_RETURN);
+ Keyboard.releaseAll();
+ delay(100);
 
-  Keyboard.press(KEY_RETURN);
-  Keyboard.releaseAll();
-  delay(100);
-
-  Keyboard.press('A');
-  Keyboard.releaseAll();
-  delay(100);
+  //Keyboard.press('A');
+ // Keyboard.releaseAll();
+ // delay(100);
 
 
 
@@ -101,7 +109,11 @@ void openMSI() {
 void openMinder() {
 
   //Open directory  
-  Keyboard.press(KEY_LEFT_GUI);
+   Keyboard.press(KEY_LEFT_GUI);
+  delay(100);
+ Keyboard.releaseAll();
+ delay(100);
+    Keyboard.press(KEY_RETURN);
   delay(100);
   Keyboard.press('R');
   delay(100);
@@ -112,44 +124,7 @@ void openMinder() {
 
  Keyboard.releaseAll();
 
- 
- delay(100);
-  Keyboard.print("C:\\Program Files (x86)\\MSI Afterburner\\MSIAfterburner.exe");
-}
 
-void moveMouseToCorner() {
-  
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  delay(100);
-  Mouse.move(127, 127);
-  
+ delay(100);
+  Keyboard.print("C:\\Users\\Stream-Pc\\Documents\\Miner\\gminer_2_70_windows64\\mine_ravencoin");
 }
